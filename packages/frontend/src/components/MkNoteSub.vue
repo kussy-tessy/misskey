02e1902d0a87ca80ec2c7f7beccc05b41e5fde44
kussy <teppei04285000@gmail.com>
2025-03-04 23:46:24 +0900

    fix

--- packages/frontend/src/components/MkNoteSub.vue
+++ packages/frontend/src/components/MkNoteSub.vue
@@ -95,7 +95,7 @@ const onPointerUp = (id: string, event: PointerEvent) => {
 
 const router = useRouter();
 function toNotePage(id: string, e: MouseEvent){
-	const ignoreSelector = "a, button, video, input, img";
+	const ignoreSelector = "a, button, video, input, .image";
 	const isIgnored = (e.target as Element).closest(ignoreSelector);
 	if (!isIgnored){
 		router.push(`/notes/${id}`);
